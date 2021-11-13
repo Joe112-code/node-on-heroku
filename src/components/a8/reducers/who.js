2ef6b32062d03ad7891b4c826491aa63eb4f99d7
Joe112-code <62823935+Joe112-code@users.noreply.github.com>
2021-11-13 00:12:53 -0500

    server of a8

--- src/components/a8/reducers/who.js
+++
@@ -1,9 +0,0 @@
-import whoJson from './data/who.json';
-
-// initialize the reducer's state
-// return the single static state
-const who = (state = whoJson) => {
-    return(state);
-};
-
-export default who;
