5f9361daa67beba37015978a02a10fbefad027cb
Joe112-code <62823935+Joe112-code@users.noreply.github.com>
2021-11-12 23:29:50 -0500

    first commit

---
+++ services/profileService.js
@@ -0,0 +1,24 @@
+const PROFILE_API = 'http://localhost:4000/api/profile';
+
+export const fetchAllProfile = (dispatch) =>
+    fetch(PROFILE_API)
+        .then(response => response.json())
+        .then(profile =>
+                  dispatch({
+                               type: 'fetch-all-profile',
+                               profile
+                           })
+        );
+
+export const updateCurrentProfile = (dispatch, profile) =>
+    fetch(PROFILE_API, {
+        method: 'PUT',
+        body: JSON.stringify(profile),
+        headers: {
+            'content-type': 'application/json'
+        }
+    }).then(response =>
+                dispatch({
+                             type: 'update-profile',
+                             profile
+                         }));
