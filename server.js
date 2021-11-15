2c3e0d9d63333b0165fd0eb9cd49988e46ae8c31
Joe112-code <62823935+Joe112-code@users.noreply.github.com>
2021-11-15 17:44:03 -0500

    test for a9

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/webdev');
+
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
@@ -19,8 +22,12 @@ app.get('/hello', (req, res) => {
     res.send('Hello World!');
 });
 // pass express instance to service
-require('./services/movies-service')(app);
-require('./services/tweet-service')(app);
-require('./services/profile-service')(app);
+// require('./services/movies-service')(app);
+// require('./services/tweet-service')(app);
+// require('./services/profile-service')(app);
+
+require('./src/a9/out-services/movies-service')(app);
+require('./src/a9/out-services/tweet-service')(app);
+require('./src/a9/out-services/profile-service')(app);
 const port = process.env.PORT || 4000;
 app.listen(port);
---
+++ src/a9/out-services/profile-service.js
@@ -0,0 +1,18 @@
+let profile = require('../reducers/data/profile.json');
+
+module.exports = (app) => {
+
+    const getCurrentProfile = (req, res) => {
+        res.json(profile);
+    }
+    app.get('/api/profile', getCurrentProfile);
+
+
+    const updateCurrentProfile = (req, res) => {
+        profile = [req.body];
+        res.json(profile);
+    }
+    app.put('/api/profile', updateCurrentProfile);
+
+
+};
